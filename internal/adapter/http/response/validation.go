8f36d3cd5d14296ebd2e31a72162c36c950f8fe8
pramesywaraj <jembarpanalar21@gmail.com>
2026-02-20 19:46:53 +0700

    feat: add error details validator translator

--- internal/adapter/http/response/validation.go
+++ internal/adapter/http/response/validation.go
@@ -10,11 +10,41 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+var tagToMessage = map[string]string{
+	"required": "is required",
+	"email":    "must be a valid email address",
+	"min":      "must be at least %s characters",
+	"max":      "must be at most %s characters",
+	"gte":      "must be at least %s",
+	"lte":      "must be at most %s",
+	"oneof":    "must be one of: %s",
+	"alpha":    "must contain only letters",
+	"alphanum": "must contain only letters and numbers",
+	"url":      "must be a valid URL",
+	"uuid":     "must be a valid UUID",
+}
+
+func getValidationMessage(e validator.FieldError) string {
+	field := e.Field()
+	tag := e.Tag()
+	param := e.Param()
+
+	msg, ok := tagToMessage[tag]
+	if !ok {
+		return fmt.Sprintf("%s: invalid", field)
+	}
+
+	if param != "" {
+		return fmt.Sprintf("%s %s", field, fmt.Sprintf(msg, param))
+	}
+	return fmt.Sprintf("%s %s", field, msg)
+}
+
 func HandleValidationError(c *gin.Context, err error) {
 	details := []string{}
 	if errs, ok := err.(validator.ValidationErrors); ok {
 		for _, e := range errs {
-			details = append(details, fmt.Sprintf("%s: %s", e.Field(), e.Tag()))
+			details = append(details, getValidationMessage(e))
 		}
 	} else {
 		details = []string{err.Error()}
