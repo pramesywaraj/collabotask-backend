6756368a0f8ddc23209f8fa1e25a9173a109dc59
pramesywaraj <jembarpanalar21@gmail.com>
2026-04-07 16:51:41 +0700

    chore: renaming variables

--- internal/adapter/http/handler/auth_handler.go
+++ internal/adapter/http/handler/auth_handler.go
@@ -30,29 +30,29 @@ func NewAuthHandler(authUseCase auth.AuthUseCase) *AuthHandler {
 // @Failure 400 {object} response.Failure400BadRequestDoc "Validation error"
 // @Failure 409 {object} response.Failure409ConflictDoc "Conflict"
 // @Router /auth/register [post]
-func (h *AuthHandler) Register(c *gin.Context) {
+func (ah *AuthHandler) Register(ctx *gin.Context) {
 	var req request.RegisterRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
-		response.HandleValidationError(c, err)
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		response.HandleValidationError(ctx, err)
 		return
 	}
 
-	out, err := h.authUseCase.Register(c.Request.Context(), auth.RegisterInput{
+	out, err := ah.authUseCase.Register(ctx.Request.Context(), auth.RegisterInput{
 		Email:    req.Email,
 		Name:     req.Name,
 		Password: req.Password,
 	})
 	if err != nil {
 		if errors.Is(err, domain.ErrEmailAlreadyExists) {
-			response.GenerateErrorResponse(c, apperrors.NewAppError(http.StatusConflict, apperrors.ErrCodeConflict, err.Error()))
+			response.GenerateErrorResponse(ctx, apperrors.NewAppError(http.StatusConflict, apperrors.ErrCodeConflict, err.Error()))
 			return
 		}
 
-		response.GenerateErrorResponse(c, apperrors.NewAppError(http.StatusConflict, apperrors.ErrCodeValidation, err.Error()))
+		response.GenerateErrorResponse(ctx, apperrors.NewAppError(http.StatusConflict, apperrors.ErrCodeValidation, err.Error()))
 		return
 	}
 
-	response.GenerateSuccessResponse(c, "User registered successfully", response.AuthResponse{
+	response.GenerateSuccessResponse(ctx, "User registered successfully", response.AuthResponse{
 		User:  response.UserDTOToResponse(out.User),
 		Token: out.Token,
 	}, http.StatusCreated)
@@ -68,23 +68,23 @@ func (h *AuthHandler) Register(c *gin.Context) {
 // @Failure 400 {object} response.Failure400ValidationDoc "Validation error"
 // @Failure 401 {object} response.Failure401LoginDoc "Unauthorized"
 // @Router /auth/login [post]
-func (h *AuthHandler) Login(c *gin.Context) {
+func (ah *AuthHandler) Login(ctx *gin.Context) {
 	var req request.LoginRequest
-	if err := c.ShouldBindJSON(&req); err != nil {
-		response.HandleValidationError(c, err)
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		response.HandleValidationError(ctx, err)
 		return
 	}
 
-	out, err := h.authUseCase.Login(c.Request.Context(), auth.LoginInput{
+	out, err := ah.authUseCase.Login(ctx.Request.Context(), auth.LoginInput{
 		Email:    req.Email,
 		Password: req.Password,
 	})
 	if err != nil {
-		response.GenerateErrorResponse(c, apperrors.NewAppError(http.StatusUnauthorized, apperrors.ErrCodeUnauthorized, err.Error()))
+		response.GenerateErrorResponse(ctx, apperrors.NewAppError(http.StatusUnauthorized, apperrors.ErrCodeUnauthorized, err.Error()))
 		return
 	}
 
-	response.GenerateSuccessResponse(c, "Successfully logged in", response.AuthResponse{
+	response.GenerateSuccessResponse(ctx, "Successfully logged in", response.AuthResponse{
 		User:  response.UserDTOToResponse(out.User),
 		Token: out.Token,
 	})
